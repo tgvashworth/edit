ebf95baf5d776b185f49e94c2ec493ea0f100b2a
Tom Ashworth <tom@phuu.net>
2012-11-24 15:29:24 +0000

    UI & sketch_name connected, not side effects.

--- app/app.js
+++ app/app.js
@@ -4,3 +4,13 @@
 
 angular.module('edit', ['source', 'preview', 'save'])
 .run(function () {});
+
+var ActionController = function ($scope, pubsub) {
+  $scope.change = function () {
+    $scope.sketch_name = ($scope.sketch_name || '')
+                            .toLowerCase()
+                            .replace(/[^a-z\s-]/g, '')
+                            .replace(/\s/g, '-')
+                            .trim();
+  };
+};
--- app/module/save.js
+++ app/module/save.js
@@ -4,13 +4,15 @@
 
 angular.module('save', ['pubsub'])
 .run(['pubsub', function (pubsub) {
+  var sketch_name = 'temp';
+
   // Intercept source responses
   pubsub.on('source:change', function (source) {
-    localStorage.setItem('edit:save:temp', source);
+    localStorage.setItem('edit:save:' + sketch_name, source);
   });
 
   // Listen for save requests and respond to
   pubsub.on('save:request', function () {
-    pubsub.emit('save:response', localStorage.getItem('edit:save:temp') || '');
+    pubsub.emit('save:response', localStorage.getItem('edit:save:' + sketch_name) || '');
   });
 }]);
